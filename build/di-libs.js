cbda9db07a40474044d06d30a82f07e8a4585b92
Gopi Krishna Suvanam <apple@Gopis-MacBook-Air.local>
2023-05-13 11:18:58 +0530

    io.js removed

--- build/di-libs.js
+++ build/di-libs.js
@@ -354,49 +354,7 @@ Array.prototype.to_series = function(column_names){
 
 
 
-})();﻿(()=>{
-
-
-const load_file= async function(){
-     
-	  const file_loader=document.createElement('input');
-	  file_id= (Math.random() + 1).toString(36).substring(7);
-	  file_loader.id= file_id;
-	  file_loader.type="file";
-	  file_loader.style.display='none';
-	  document.body.appendChild(file_loader);
-
-	await new Promise(resolve => setTimeout(resolve, 200));
-
-	x= await new Promise(resolve => {
-	 get_dom(file_id).addEventListener("change",event => {
-
-	 const fr = new FileReader();
-
-
-			 fr.onload=async function(event){
-			   		content=event.target.result;
-			   		get_dom(file_id).remove();
-			   		resolve(content);
-
-			 };
-
-	   fr.readAsText( get_dom(file_id).files[0]);
-
-
-
-	  });
-
-
-		 get_dom(file_id).click();
-	});
-  return(x);
- 
-}
-
-window.load_file=load_file;
-})();
-  // Take dataframe as dataframejs or 2D array or X and Y as arrays
+})();// Take dataframe as dataframejs or 2D array or X and Y as arrays
 // Following plots:
 // pie(X,Y, layout). 
 // bar(labels,[Ys],layout)
