3e19f4c20bcff696a01b2d65ad4d3234adbffff2
Gopi Krishna Suvanam <gopi.suvanam@gmail.com>
2023-11-29 16:26:02 +0530

    non linear

--- build/di-libs.js
+++ build/di-libs.js
@@ -7134,7 +7134,7 @@ function multivariateLinearRegression(xValues, yValues) {
   // Compute X'X and X'y
 
   const xTx = transposedX.mmult(centeredX);
-  const xTy = transposedX.map((row, i) => row.reduce((acc, val) => acc + val * yValues[i], 0));
+  const xTy = transposedX.map(row => numeric.dot(row,centeredY));
 
   // Calculate the coefficients (beta)
   const coefficients = numeric.solve(xTx, xTy);
@@ -7142,7 +7142,6 @@ function multivariateLinearRegression(xValues, yValues) {
   
   return [constant,...coefficients];
 }
-
 
 
 ml.NonLinearLS=function(activation){
@@ -7179,6 +7178,73 @@ ml.NonLinearLS=function(activation){
 	}
 
 }
+
+
+ml.NonLinearEnsemble=function(activations,feature_selection_prob,sample_selection_prob,num_estimators,meta_activation){
+	this.activations=activations;
+	this.feature_selection_prob=feature_selection_prob;
+	this.sample_selection_prob=sample_selection_prob;
+	this.models=[];
+	this.num_estimators=num_estimators;
+	this.model_weights=[];
+	this.vars=[];
+	this.meta_model = null;
+	this.meta_activation=meta_activation;
+	
+	this.fit=function(X,Y,vars){
+		if (vars==undefined)
+			this.vars=Object.keys(X[0]);
+		else
+			this.vars=vars;
+		const num_samples = X.length;
+		const index=range(0,num_samples);
+		var errors=[];
+		for(let model_no=0;model_no<num_estimators;model_no++){
+			try{
+				const model_activation = this.activations[Math.floor(this.activations.length * Math.random())]
+				const model_vars = this.vars.filter(_=>Math.random()<this.feature_selection_prob);
+				const model_samples= index.filter(_=>Math.random()<this.sample_selection_prob);
+				const model_X= model_samples.map(i=>X[i]);
+				const model_Y= model_samples.map(i=>Y[i]);
+				var model=new ml.NonLinearLS(model_activation);
+				model.fit(model_X,model_Y,model_vars);
+			  let err=error.rms(model_Y,model.predict(model_X));
+			  if(err>0)
+				this.models.push(model);
+			}catch(e){
+				console.log("Failed for model_no",model_no,e);
+			}
+		}
+		
+		const meta_model_samples= index.filter(_=>Math.random()<this.sample_selection_prob);
+		const meta_model_X= meta_model_samples.map(i=>X[i]);
+		const meta_X = this.models.map(model=>model.predict(meta_model_X)).transpose();
+		
+		const meta_Y= meta_model_samples.map(i=>Y[i]);
+		
+			
+		this.meta_model = new ml.NonLinearLS(meta_activation);
+		this.meta_model.fit(meta_X,meta_Y);
+		
+		
+	}
+	this.predict=function(X){
+		num_estimators=this.models.length;
+		var meta_model_X = [];
+		for(let model_no=0;model_no<num_estimators;model_no++){
+			const Y_model = this.models[model_no].predict(X);
+			meta_model_X.push(Y_model);
+		}
+		meta_model_X=meta_model_X.transpose();
+		const Y_pred=this.meta_model.predict(meta_model_X);
+		return Y_pred;
+	
+	}
+	
+	
+}
+
+
 
 })()/* functions to upload files to p2p.. network and download.
 It is a layer on top of Webtorrents/ipfs-js etc */
