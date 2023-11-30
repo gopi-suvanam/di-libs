1df500a03323503504561b8d78039cf2874b87c3
Gopi Krishna Suvanam <gopi.suvanam@gmail.com>
2023-11-30 14:38:05 +0530

    debug previous

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find ./src -name '*.js' -exec cat {} \; | sed 's/$/;/' > ./build/di-libs.js
+find ./src -name '*.js' -exec sh -c 'cat {} && echo ";\n"' \; > ./build/di-libs.js
 cat ./build/di-libs.js | sed -e "s|/\*\(\\\\\)\?\*/|/~\1~/|g" -e "s|/\*[^*]*\*\+\([^/][^*]*\*\+\)*/||g" -e "s|\([^:/]\)//.*$|\1|" -e "s|^//.*$||" | tr '\n' ' ' | sed -e "s|/\*[^*]*\*\+\([^/][^*]*\*\+\)*/||g" -e "s|/\~\(\\\\\)\?\~/|/*\1*/|g" -e "s|\s\+| |g" -e "s| \([{;:,]\)|\1|g" -e "s|\([{;:,]\) |\1|g" > ./build/di-libs.min.js
 echo
