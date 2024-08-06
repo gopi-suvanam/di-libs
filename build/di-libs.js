ab26a98374037123637718ecad679cbc2f4999e1
Gopi Krishna Suvanam <gopi.suvanam@gmail.com>
2024-08-06 15:10:08 +0530

    numerical methods

--- build/di-libs.js
+++ build/di-libs.js
@@ -6568,14 +6568,14 @@ if(typeof di=== 'undefined') di={};
 
 let activation = {};
 activation.linear = (x=>x);
-activation.linear.inverse = ()=>(x=>x);
+activation.linear.inverse = (x=>x);
 
 activation.softstep = (x=> //1/(1+e^(-x))
 			  x>0?
 			  1/(1+Math.exp(-x)):
 			  Math.exp(x)/(1+Math.exp(x))
 			  );
-activation.softstep.inverse = ()=>(p=>-Math.log((1-p)/p));
+activation.softstep.inverse = (p=>-Math.log((1-p)/p));
  
 
 activation.softplus = (x=> x>0?   //log(1+e^(x))
@@ -6583,9 +6583,10 @@ activation.softplus = (x=> x>0?   //log(1+e^(x))
 	   		Math.log(1+Math.exp(x))
 					  
 			);
-activation.softplus.inverse =()=>(y=>Math.log(1-Math.exp(-y)) + y )
+activation.softplus.inverse =(y=>Math.log(1-Math.exp(-y)) + y )
 
-
+activation.exp= x=>Math.exp(x);
+activation.exp.inverse=y=>Math.log(y);	 
 
 const k=1;
 activation.softlog = (x=>
@@ -6596,7 +6597,8 @@ activation.softlog = (x=>
 di.activation=activation;
 
 
-})();;
+})();
+;
 
 ﻿(()=>{
 
